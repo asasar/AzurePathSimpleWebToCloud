6bd339687c06ed1adf643ac1b0d54fb3aeb3af56
alexis@ <alexis@>
2018-03-28 16:44:31 +0200

    Init simple WebApp dotnet Core with MVC Angular

---
+++ src-Step001/WebAppSimple/ClientApp/app/components/navmenu/userLogin/userlogin.component.ts
@@ -0,0 +1,33 @@
+﻿import { Component, Inject} from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'user-login',
+    templateUrl: './userlogin.component.html',
+    styleUrls: ['./userlogin.component.css']
+})
+export class UserLoginComponent {
+
+    public userInfo: any;
+    
+
+    public nonConnected = false;
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'account/MeInfo').subscribe(result => {
+            console.log( "Result Call");
+            this.userInfo = result.json();
+
+            console.log(result.json());
+            console.log(this.userInfo, "from userInfo");    
+            this.nonConnected = false;
+           
+
+        }, error => {
+            console.error(error, "Error");
+            this.nonConnected = true;
+
+            });
+    }
+
+}
---
+++ src-Step001/WebAppSimple/Startup.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ngCore.Data;
+using ngCore.Models;
+using ngCore.Services;
+namespace ngCore
+{
+    public class Startup
+    {
+        const string defaultConnection = "DefaultConnection";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            if (Configuration.GetConnectionString(defaultConnection).ToLower().Contains("azure"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString(defaultConnection)));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlite(Configuration.GetConnectionString(defaultConnection)));
+            }
+           
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                options => {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredLength = 4;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+                //{
+                //    HotModuleReplacement = true
+                //});
+
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+
+
+            app.UseStaticFiles();
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
